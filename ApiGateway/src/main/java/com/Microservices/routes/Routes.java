5abf6d1c5485d1c7ae6b0e9825f9816fd39da687
Zisx96 <yashnikhurpa96@gmail.com>
2024-12-30 16:36:31 +0530

    new commit

--- ApiGateway/src/main/java/com/Microservices/routes/Routes.java
+++ ApiGateway/src/main/java/com/Microservices/routes/Routes.java
@@ -1,9 +1,13 @@
 package com.Microservices.routes;
 
+import java.net.URI;
+
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
@@ -17,8 +21,21 @@ public class Routes {
 		return GatewayRouterFunctions.route("MICROAPP1")
 				.route(RequestPredicates.path("/info"), 
 						HandlerFunctions.http("http://localhost:8482"))
+				.filter(CircuitBreakerFilterFunctions
+						.circuitBreaker("MICROAPP1CircuitBreaker",
+								URI.create("forward:/fallbackRouter")))
 				.build();
 	}
+	
+	@Bean
+	public RouterFunction<ServerResponse> fallbackRouter(){
+		
+		return GatewayRouterFunctions.route("fallbackRouter")
+				.GET("/fallbackRouter", request-> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+						.body("Service Currently Unavailable, try again after some time"))
+				.build();
+	}
+	
 	
 	@Bean
 	public RouterFunction<ServerResponse> service2(){
